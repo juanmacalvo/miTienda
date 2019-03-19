da5b124f0d636b0bceb609ba360d840314c37d27
usuario <usuario@pc21-094>
2019-03-19 16:48:55 +0100

    Implementación clase Artículo

--- miTienda/src/main/java/tienda/Articulo.java
+++ miTienda/src/main/java/tienda/Articulo.java
@@ -10,5 +10,41 @@ package tienda;
  * @author usuario
  */
 public class Articulo {
+
+    private int cantidad;
+    private int pvp;
+    private int id;
+
+    public Articulo(int cantidad, int pvp, int id) {
+        this.cantidad = cantidad;
+        this.pvp = pvp;
+        this.id = id;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public int getPvp() {
+        return pvp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setPvp(int pvp) {
+        this.pvp = pvp;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
     
+    
+
 }
