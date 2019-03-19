15e8a656f46855ebcf0ad8d3f394ce8dd3792d90
acebesalv <48252355+acebesalv@users.noreply.github.com>
2019-03-19 17:11:02 +0100

    Update compra.java
    Actualizacion compra

--- miTienda/src/main/java/tienda/Compra.java
+++ miTienda/src/main/java/tienda/Compra.java
@@ -10,5 +10,15 @@ package tienda;
  * @author usuario
  */
 public class Compra {
+    Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca id de articulo: ");
+        int id = sc.nextInt();
+        System.out.println("Introduzca pvp de articulo: ");
+        int pvp = sc.nextInt();
+        System.out.println("Introduzca cantidad de articulo: ");
+        int cantidad = sc.nextInt();
+        Articulo a = new Articulo (cantidad, pvp, id, cantidad);
+        A.add(a);
+    
     
 }
