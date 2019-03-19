85fdf7987ab127f3ecbb43b4db7b97ca98b50708
CarmenRC <carmenrc2594@gmail.com>
2019-03-19 17:08:31 +0100

    Update Venta.java

--- miTienda/src/main/java/tienda/Venta.java
+++ miTienda/src/main/java/tienda/Venta.java
@@ -5,10 +5,46 @@
  */
 package tienda;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author usuario
  */
 public class Venta {
     
+    private Articulo art;
+
+    
+    
+    public Venta(Articulo art) {
+        this.art = art;
+    }
+       
+    public void vender (ArrayList<Articulo> articulos)
+    {
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Introduzca el nombre del ID a vender: ");
+        int id = sc.nextInt();
+        for (int i = 0; i < articulos.size(); i++) {
+            if(articulos.get(i).getId() == id)
+            {                
+                articulos.get(i).setStock(articulos.get(i).getStock()-1);
+                if(articulos.get(i).getStock()==0)
+                {
+                    articulos.remove(i);//
+                }
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
 }
+
